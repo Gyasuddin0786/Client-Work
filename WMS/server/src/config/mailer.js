17f1d0599d686076a8951450b0e56647c57071f4
Gyasuddin <gyasuddinansari7854@gmail.com>
2026-08-06 10:33:42 +0530

    mail.js file updated with deployement port

--- WMS/server/src/config/mailer.js
+++ WMS/server/src/config/mailer.js
@@ -4,11 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
